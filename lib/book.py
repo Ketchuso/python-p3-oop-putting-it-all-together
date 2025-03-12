65fa69bbb292d8c3d4bd0f5b4e9909f2f1fb0c22
Jay Davis <jdlikesbsbl@gmail.com>
2025-03-12 13:33:08 -0500

    completes lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(book, title, page_count):
+        book.title = title
+        book.page_count = page_count
+
+    @property
+    def page_count(book):
+        return book._page_count 
+
+    @page_count.setter
+    def page_count(book, value):
+        if not isinstance(value, int):  
+            print("page_count must be an integer")
+        book._page_count = value
+
+    def turn_page(self):
+        print ("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(shoe, brand, size):
+        shoe.brand = brand
+        shoe.size = size
+    
+    @property
+    def size(shoe):
+        return shoe._size
+    
+    @size.setter
+    def size(shoe, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        shoe._size = value
+    
+    def cobble(shoe):
+        shoe.condition = "New"
+        print("Your shoe is as good as new!")
